4c6df04fdb6d131359e960faaa821e73a7696791
Sai Panneerselvam <89541946+SaiPanneerselvam@users.noreply.github.com>
2022-07-05 21:08:56 -0400

    Python Build 001

---
+++ bin/installApexGaming.py
@@ -0,0 +1,6 @@
+import minebuild
+
+py_begin()
+print("Welcome to the Apex Gaming.")
+py_apextools_install_begin()
+py_end()
---
+++ bin/minebuild.py
@@ -0,0 +1,8 @@
+def py_begin():
+    py_begin()
+def py_end():
+    py_end()
+def py_apextools_install_begin():
+    print("ApexTools Installation has begun.")
+    time.sleep(5)
+    print('Installation Complete. Package "ApexTools" Installation is now done.')
